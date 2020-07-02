2da89b7839c76576fc739a61edd7d2021392f382
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-07-02 16:02:43 -0400

    completed

--- Loops/index.js
+++ Loops/index.js
@@ -2,15 +2,12 @@
 PLEASE DO NOT TOUCH THE TEST SECTION
 
 PLEASE USE WHILE-LOOPS OR FOR-LOOPS
-
 */
 
 /*
 EXAMPLE
 
 CREATE A FUNCTION THAT CONSOLE.LOGS hello world 3 times
-
-
 */
 
 function helloWorld3Times() {
@@ -19,67 +16,88 @@ function helloWorld3Times() {
   }
 }
 
+
+
 /*
 QUESTION 1
 
 CREATE A FUNCTION THAT ACCEPTS ONE ARGUMENT, THE NUMBER OF TIMES IT WILL CONSOLE.LOG hello world
-
-
 */
 
 function dynamicHelloWorld(num) {
   // CODE HERE
+  for(let i=0; i<num; i++){
+    console.log('hello world');
+  }
 }
+
+
 
 /*
 QUESTION 2
 
 CREATE A FUNCTION THAT ACCEPTS A STRING AND WILL LOG TO THE CONSOLE EACH CHARACTER OF THE STRING
-
 */
 
 function logEachChar(str) {
   // CODE HERE
+  for(let i=0; i<str.length; i++){
+    console.log(str[i]);
+  }
 }
+
+
 
 /*
 QUESTION 3
 
-CREATE A FUNCTION THAT ACCEPTS TWO STRINGS. ONE STRING IS A WORD, THE OTHER STRING IS A SINGLE CHARACTER. THE FUNCTION WILL RETURN THE BOOLEAN true IF THAT CHARACTER EXISTS IN THE FUNCTION OTHERWISE WILL DEFAULT TO false
+CREATE A FUNCTION THAT ACCEPTS TWO STRINGS. ONE STRING IS A WORD, THE OTHER STRING IS A SINGLE CHARACTER. 
+THE FUNCTION WILL RETURN THE BOOLEAN true IF THAT CHARACTER EXISTS IN THE FUNCTION 
+OTHERWISE WILL DEFAULT TO false
 
 PLEASE USE LOOPS AND NOT string.indexOf()
 
-e.g. checkIfCharExists('hello', 'l') ===> true
-
+checkIfCharExists('hello', 'l') ===> true
 */
 
 function checkIfCharExists(str, char) {
   // CODE HERE
+  return(str.includes(char));
 }
+
+
 
 /*
 QUESTION 4
 
-CREATE A FUNCTION THAT TAKES TWO ARGUMENTS, A WORD AND A CHARACTER, IT THEN RETURNS THE INDEX OF THE FIRST INSTANCE OF THAT CHARACTER'S LOCATION. IF IT DOES NOT EXIST IN THE WORD RETURN -1
+CREATE A FUNCTION THAT TAKES TWO ARGUMENTS, A WORD AND A CHARACTER, 
+IT THEN RETURNS THE INDEX OF THE FIRST INSTANCE OF THAT CHARACTER'S LOCATION. 
+IF IT DOES NOT EXIST IN THE WORD RETURN -1
+DO NOT USE .indexOf()!!!
 
-
-e.g. indexOf('hello', 'l') ===> 2
-
+indexOf('hello', 'l') ===> 2
 */
 
 function indexOf(str, char) {
   // CODE HERE
+  for(let i=0; i<str.length; i++){
+    if(str[i]===char) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 /*
 QUESTION 5
 
-CREATE A FUNCTION THAT TAKES ONE ARGUMENTS, A NUMBER,  AND CONSOLE.LOGS THE NUMBERS 1 to ARGUMENT. BUT FOR MULTIPLES OF THREE CONSOLE.LOG "Fizz" INSTEAD OF THE NUMBER AND FOR THE MULTIPLES OF FIVE CONSOLE.LOG "Buzz". FOR NUMBERS WHICH ARE MULTIPLES OF BOTH THREE AND FIVE PRINT "FizzBuzz".
-
+CREATE A FUNCTION THAT TAKES ONE ARGUMENTS, A NUMBER,  AND CONSOLE.LOGS THE NUMBERS 1 to ARGUMENT. 
+BUT FOR MULTIPLES OF THREE CONSOLE.LOG "Fizz" INSTEAD OF THE NUMBER AND 
+FOR THE MULTIPLES OF FIVE CONSOLE.LOG "Buzz". 
+FOR NUMBERS WHICH ARE MULTIPLES OF BOTH THREE AND FIVE PRINT "FizzBuzz".
 
 e.g. indexOf(15)
 OUTPUT:
-
 1
 2
 Fizz
@@ -95,36 +113,63 @@ Fizz
 13
 14
 FizzBuzz
-
-
 */
 
 function fizzBuzz(num) {
   // CODE HERE
+  if(typeof(num)!=='number' || num<=0) {
+    console.log('Invalid Input');
+  }
+  else {
+    for(let i=1; i<=num; i++){
+      if(i%15===0) console.log('FizzBuzz');
+      else if(i%3===0) console.log('Fizz');
+      else if(i%5===0) console.log('Buzz');
+      else console.log(i);
+    }
+  }
 }
+
+
 
 /*
 QUESTION 6
-CREATE A FUNCTION THAT TAKES ONE ARGUMENTS, A NUMBER,  AND FIND'S THE   nth FIBONACCI NUMBER.
-THE FIBONACCI SEQUENCE STARTS WITH 1 AND 1 AND THEN THE NEXT NUMBER IS THE ADDITION OF THE PREVIOUS TWO NUMBERS.
+CREATE A FUNCTION THAT TAKES ONE ARGUMENTS, A NUMBER,  AND FIND'S THE nth FIBONACCI NUMBER.
+THE FIBONACCI SEQUENCE STARTS WITH 1 AND 1 AND THEN 
+THE NEXT NUMBER IS THE ADDITION OF THE PREVIOUS TWO NUMBERS.
 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, …
-
-
-
 */
 
 function fibonacci(n) {
   // CODE HERE
+  let fib=[1, 1];
+  
+  if (typeof(n) !== 'number' || n<0) return 'Invalid input';
+
+  for(let i=2; i<n; i++){
+    fib.push(fib[i-1]+fib[i-2]);
+  }
+  return fib[n-1];
+
+  /* 
+  RECURSION
+  // TERMINATING CASE
+  if (n<0) return 'invalid input';
+
+  // BASE CASE
+  if (n<=2) return 1;
+
+  // RECURSION
+  return fibonacci(n-1)+fibonacci(n-2);
+  */
 }
 
 console.log(fibonacci(5));
 
+
+
 /*
 TEST SECTION, PLEASE DO NOT TOUCH
-
-
-
-
 */
 
 const Mocha = require('mocha');
